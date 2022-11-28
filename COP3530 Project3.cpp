e7fb14a29ba0dfe66196495c76689533502f6174
benmendoza3 <b.mendoza@ufl.edu>
2022-11-28 16:15:35 -0500

    Updated main to have if-else for existing users in both maps, and using the ReviewList class for both maps as well.

--- COP3530 Project3.cpp
+++ COP3530 Project3.cpp
@@ -29,52 +29,84 @@ int main() {
 	testList.radixSort();
 	testList.printSortTime();
 
-	//indicators::show_console_cursor(false);
+	indicators::show_console_cursor(false);
 
-	//// start chrono timer
-	//auto start = chrono::high_resolution_clock::now();
-	//unordered_map<string, unordered_map<int, float>> userRatings;
+	// start chrono timer
+	auto start = chrono::high_resolution_clock::now();
 
-	//CSVReader reader("user_ratings.csv");
-	//CSVRow row;
-	//std::cout << "Loaded file." << std::endl;
-	//
-	//// progress bar
-	//ProgressBar bar;
-	//bar.set_option(option::BarWidth{ 70 });
-	//bar.set_option(option::Start{ "[" });
-	//bar.set_option(option::Fill{ "=" });
-	//bar.set_option(option::Lead{ "=" });
-	//bar.set_option(option::Remainder{ " " });
-	//bar.set_option(option::End{ " ]" });
-	//bar.set_option(option::ShowPercentage{ true });
-	//bar.set_option(option::ShowElapsedTime{ true });
-	//bar.set_option(option::ShowRemainingTime{ true });
-	//bar.set_option(option::ForegroundColor{ Color::white });
-	//bar.set_option(option::FontStyles{ std::vector<FontStyle>{FontStyle::bold} });
-	//bar.set_option(option::MaxProgress{ 100 });
+    // unordered map
+    unordered_map<string, ReviewList> rawUserRatings;
 
-	//
-	//int i = 0;
-	//for (auto& row : reader) {
-	//	string user = row["Username"];
-	//	int game = row["BGGId"].get<int>();
-	//	float rating = row["Rating"].get<float>();
-	//	userRatings[user].insert(std::make_pair(game, rating));
-	//	userRatings[user][game] = rating;
+    // ordered map
+    map<string, ReviewList> sortedUserRatings;
 
-	//	if (i % 189423 == 0) {
-	//		bar.tick();
-	//	}
-	//	i++;
-	//}
+	CSVReader reader("user_ratings.csv");
+    CSVRow row;
+	std::cout << "Loaded file." << std::endl;
 
-	//bar.mark_as_completed();
+	// progress bar
+	ProgressBar bar;
+	bar.set_option(option::BarWidth{ 70 });
+	bar.set_option(option::Start{ "[" });
+	bar.set_option(option::Fill{ "=" });
+	bar.set_option(option::Lead{ "=" });
+	bar.set_option(option::Remainder{ " " });
+	bar.set_option(option::End{ " ]" });
+	bar.set_option(option::ShowPercentage{ true });
+	bar.set_option(option::ShowElapsedTime{ true });
+	bar.set_option(option::ShowRemainingTime{ true });
+	bar.set_option(option::ForegroundColor{ Color::white });
+	bar.set_option(option::FontStyles{ std::vector<FontStyle>{FontStyle::bold} });
+	bar.set_option(option::MaxProgress{ 100 });
 
-	//indicators::show_console_cursor(true);
 
-	//auto end = chrono::high_resolution_clock::now();
-	//auto duration = chrono::duration_cast<chrono::seconds>(end - start);
-	//std::cout << "Time: " << duration.count() << " seconds" << endl;
-	//return 0;
+	int i = 0;
+	for (auto& row : reader) {
+		string user = row["Username"];
+		int game = row["BGGId"].get<int>();
+		float rating = row["Rating"].get<float>();
+
+        // Creating a new review object, to add to already existing Player's ReviewList, or to add to the
+        // corresponding new Player's
+        ReviewList::ReviewItem newItem(game, user, rating, "");
+
+        // Adding to unsorted map, plain and simple
+        if (rawUserRatings.find(user) == rawUserRatings.end())
+        {
+            ReviewList newList = ReviewList();
+            newList.addReview(newItem);
+            rawUserRatings.insert(std::make_pair(user, newList));
+        }
+        else
+        {
+            rawUserRatings[user].addReview(newItem);
+        }
+
+        // Checking sorted reviews for the user's name
+        if (sortedUserRatings.find(user) == sortedUserRatings.end())
+        {
+            // If not found, create a new list, add the new review to the list, and add the user and their ReviewList
+            // to our map of player's and their reviews.
+            ReviewList newList = ReviewList();
+            newList.addReview(newItem);
+            sortedUserRatings.insert(std::make_pair(user, newList));
+        }
+        else
+        {
+            // If found, just add the review to the already existing spot in our map with the player's name
+            sortedUserRatings[user].addReview(newItem);
+        }
+
+		if (i % 189423 == 0) {
+			bar.tick();
+		}
+		i++;
+	}
+
+	indicators::show_console_cursor(true);
+
+	auto end = chrono::high_resolution_clock::now();
+	auto duration = chrono::duration_cast<chrono::seconds>(end - start);
+	std::cout << "Time: " << duration.count() << " seconds" << endl;
+	return 0;
 }
